b8456a8143cd9bf108c07ddec284a69cb659b455
Bijaya Subedi <beejaysuvedi@gmail.com>
2026-09-25 14:13:40 +0545

    refactor: improve FuelController structure and readability

--- Assets/Scripts/FuelController.cs
+++ Assets/Scripts/FuelController.cs
@@ -18,7 +18,7 @@ public class FuelController : MonoBehaviour
         {
             Instance = this;
         }
-        else
+        else if (Instance != this)
         {
             Destroy(gameObject);
         }
@@ -30,19 +30,33 @@ public class FuelController : MonoBehaviour
         UpdateFuelUI();
     }
 
-    private void UpdateFuelUI()
-{
-    float fuelPercent = currentFuelAmount / maxFuelAmount;
-
-    fuelFrontImage.fillAmount = fuelPercent;
-    fuelFrontImage.color = fuelGradient.Evaluate(fuelPercent);
-}
-
     private void Update()
     {
+        if (GameManager.Instance.IsGameOver) return;
+
         currentFuelAmount -= drainSpeed * Time.deltaTime;
         currentFuelAmount = Mathf.Max(currentFuelAmount, 0f);
 
+        UpdateFuelUI();
+
+        // End the game when fuel is empty.
+        if (currentFuelAmount <= 0f)
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
+
+    private void UpdateFuelUI()
+    {
+        float fuelPercent = currentFuelAmount / maxFuelAmount;
+
+        fuelFrontImage.fillAmount = fuelPercent;
+        fuelFrontImage.color = fuelGradient.Evaluate(fuelPercent);
+    }
+
+    public void FillFuel()
+    {
+        currentFuelAmount = maxFuelAmount;
         UpdateFuelUI();
     }
 }
